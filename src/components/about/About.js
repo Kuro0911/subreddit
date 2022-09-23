9a115227f8a7e1270b6ed9852dad480e315983e7
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-09-24 04:41:37 +0530

    added about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,56 @@
 import React from "react";
-import { AboutWrap } from "./About.style";
+import {
+  AboutWrap,
+  CountWrap,
+  FooterWrap,
+  Slug,
+  TagWrap,
+  TextWrap,
+  TopWrap,
+} from "./About.style";
+import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
+import CakeOutlinedIcon from "@mui/icons-material/CakeOutlined";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 
 export const About = () => {
   return (
     <AboutWrap>
-      <h1>ABOUT</h1>
+      <TopWrap>
+        <h4>About Community</h4>
+        <MoreHorizOutlinedIcon className="icon" />
+      </TopWrap>
+      <TextWrap>
+        Svelte is a radical new approach to building user interfaces. Whereas
+        traditional frameworks like React and Vue do the bulk of their work in
+        the browser, Svelte shifts that work into a compile step that happens
+        when you build your app. Instead of using techniques like virtual DOM
+        diffing, Svelte writes code that surgically updates the DOM when the
+        state of your app changes.
+      </TextWrap>
+      <CountWrap>
+        <div className="left">
+          <h2>17.5k</h2>
+          <span>Members</span>
+        </div>
+        <div className="right">
+          <h2>60</h2>
+          <span>Online</span>
+        </div>
+      </CountWrap>
+      <Slug />
+      <FooterWrap>
+        <div className="left">
+          <CakeOutlinedIcon />
+          <span>Created Dec 3, 2016</span>
+        </div>
+        <div className="right">
+          <LocalOfferIcon />
+          <span>r/sveltejs topics</span>
+        </div>
+      </FooterWrap>
+      <TagWrap>
+        <span>Programming</span>
+      </TagWrap>
     </AboutWrap>
   );
 };
--- src/components/about/About.style.js
+++ src/components/about/About.style.js
@@ -1,9 +1,92 @@
 import styled from "styled-components";
 
-export const AboutWrap = styled.div`
+const AboutWrap = styled.div`
   width: 20%;
-  height: 50vh;
   background-color: white;
   margin-top: 1em;
   margin-left: -18%;
+  height: fit-content;
 `;
+const TopWrap = styled.div`
+  background-color: #ff4500;
+  height: 4em;
+  width: 100%;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h4 {
+    margin-left: 1em;
+  }
+  .icon {
+    margin-right: 1em;
+  }
+`;
+const TextWrap = styled.div`
+  text-align: left;
+  font-family: Noto Sans, Arial, sans-serif;
+  font-size: 15px;
+  line-height: 21px;
+  font-weight: 400;
+  word-wrap: break-word;
+  margin: 1em;
+  color: #444648;
+`;
+const CountWrap = styled.div`
+  display: flex;
+  line-height: 20px;
+  text-align: left;
+  width: 100%;
+  color: #2b2c2d;
+  .left {
+    margin-left: 1em;
+  }
+  .right {
+    margin-left: 4em;
+  }
+  h2 {
+    font-size: 16px;
+    font-weight: 500;
+  }
+`;
+const Slug = styled.div`
+  width: 100%;
+  background-color: rgba(26, 26, 27, 0.07);
+  height: 1.7px;
+  margin-top: 1em;
+`;
+const FooterWrap = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  margin-bottom: 1em;
+  color: #2b2c2d;
+
+  .left {
+    display: flex;
+    align-items: flex-start;
+    margin-left: 1em;
+    margin-top: 0.75em;
+  }
+  .right {
+    display: flex;
+    align-items: flex-start;
+    margin-top: 0.75em;
+    margin-left: 1em;
+  }
+  span {
+    margin-left: 0.65em;
+  }
+`;
+const TagWrap = styled.div`
+  border-radius: 20px;
+  height: 2em;
+  width: 8em;
+  background-color: #ff4500;
+  color: white;
+  text-align: center;
+  margin-left: 1em;
+  margin-top: -0.5em;
+  margin-bottom: 0.5em;
+`;
+export { AboutWrap, TopWrap, TextWrap, CountWrap, Slug, FooterWrap, TagWrap };
