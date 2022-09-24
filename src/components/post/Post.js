f633dc99f829ad8dafef556f9a47dffdcc1d3a9c
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-09-24 13:26:31 +0530

    added votes

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { PostWrap } from "./Post.style";
+import ForwardOutlinedIcon from "@mui/icons-material/ForwardOutlined";
 
 export const Post = () => {
+  const [votes, setVotes] = useState(10);
+  const [active, setActive] = useState("");
+  const handleClick = (flag) => {
+    console.log(flag);
+    if (flag === "up" && active !== "up") {
+      setVotes(votes + 1);
+      setActive("up");
+    } else if (flag === "up" && active === "up") {
+      setVotes(votes - 1);
+      setActive("");
+    } else if (flag === "down" && active !== "down") {
+      setVotes(votes - 1);
+      setActive("down");
+    } else {
+      setVotes(votes + 1);
+      setActive("");
+    }
+  };
   return (
-    <PostWrap>
-      <h1>post</h1>
+    <PostWrap active={active}>
+      <div className="vote">
+        <ForwardOutlinedIcon
+          className={active === "up" ? "upvote-act" : "upvote"}
+          onClick={() => {
+            handleClick("up");
+          }}
+        />
+        <div className="number">{votes}</div>
+        <ForwardOutlinedIcon
+          className={active === "down" ? "downvote-act" : "downvote"}
+          onClick={() => {
+            handleClick("down");
+          }}
+        />
+      </div>
+      <div className="body"></div>
     </PostWrap>
   );
 };
--- src/components/post/Post.style.js
+++ src/components/post/Post.style.js
@@ -5,4 +5,52 @@ export const PostWrap = styled.div`
   height: 30vh;
   background-color: white;
   margin-bottom: 0.75em;
+  display: flex;
+  .vote {
+    height: 100%
+    width: 5%;
+    .number{
+    font-size: 12px;
+    font-weight: 700;
+    margin-top: 0.25em;
+    margin-bottom: 0.25em;
+    color: ${(props) =>
+      props.active === "up"
+        ? "#ff6a33"
+        : props.active === "down"
+        ? "#7193ff"
+        : "black"};
+    }
+    .upvote{      
+      transform: rotateZ(270deg);
+      color: #7c7c7c;
+      cursor: pointer;
+    }
+    .upvote-act{      
+      transform: rotateZ(270deg);
+      background-color: #ff6a33;
+      cursor: pointer;
+    }
+    .upvote:hover{
+      color: #ff6a33;
+    }
+    .downvote{
+      transform: rotateZ(90deg);
+      color: #7c7c7c;
+      cursor: pointer;
+    }
+    .downvote-act{      
+      transform: rotateZ(90deg);
+      background-color: #7193ff;
+      cursor: pointer;
+    }
+    .downvote:hover{
+      color: #7193ff;
+    }
+  }
+  .body {
+    background-color: red;
+    height: 100%
+    width: 95%;
+  }
 `;
